d277f899eb77a195b05af4e4afc19dd4194cbd55
AlexGarcia0 <117188682+AlexGarcia0@users.noreply.github.com>
2024-01-15 18:44:46 +0100

    pruebas unitarias JUnit

---
+++ JUnits/bd/BaseDatosTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package bd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import domain.Administrador;
+import domain.Entrenamiento;
+import domain.Monitor;
+import domain.Suscripcion;
+import domain.TipoSuscripcion;
+import domain.TiposEntrenamientos;
+import domain.Usuario;
+
+public class BaseDatosTest {
+	
+}
